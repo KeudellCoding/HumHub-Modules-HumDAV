d45a6baa6de08a2eb2d116d9c73dc022b2ce2d3d
KarlKeu00 <47644344+KarlKeu00@users.noreply.github.com>
2020-07-03 14:35:24 +0200

    Removing VCF string

--- definitions/VCardDefinitions.php
+++ definitions/VCardDefinitions.php
@@ -18,7 +18,7 @@ class VCardDefinitions {
 
         $profile = $user->profile;
 
-        $vCard = <<<VCF
+        $vCard = "
         BEGIN:VCARD\r\n
         VERSION:4.0\r\n
         KIND:individual\r\n
@@ -26,21 +26,21 @@ class VCardDefinitions {
         FN:$user->displayname\r\n
         N:$profile->lastname;$profile->firstname;;;\r\n
         EMAIL:$user->email\r\n
-        VCF;
+        ";
 
         if ((boolean)$settings->get('include_address', true) === true) {  // Adding Address
-            $vCard .= <<<VCF
+            $vCard .= "
             ADR;TYPE=home:;;$profile->street;$profile->city;$profile->state;$profile->zip;$profile->country\r\n
-            VCF;
+            ";
         }
 
         if ((boolean)$settings->get('include_profile_image', true) === true) {  // Add Profile Image
             $profileImage = new ProfileImage($user->guid);
             if ($profileImage->hasImage()) {
                 $profileImageUrl = $profileImage->getUrl('', true);
-                $vCard .= <<<VCF
+                $vCard .= "
                 PHOTO:$profileImageUrl\r\n
-                VCF;
+                ";
             }
         }
         
@@ -54,68 +54,68 @@ class VCardDefinitions {
                 else {
                     $birthdayFormated = $birthdayDatetime->format('Ymd');
                 }
-                $vCard .= <<<VCF
+                $vCard .= "
                 BDAY:$birthdayFormated\r\n
-                VCF;
+                ";
             }
         }
         
         if ((boolean)$settings->get('include_gender', true) === true) {  // Add Gender
             if ($profile->gender === 'male') {
-                $vCard .= <<<VCF
+                $vCard .= "
                 GENDER:M\r\n
-                VCF;
+                ";
             }
             else if ($profile->gender === 'female') {
-                $vCard .= <<<VCF
+                $vCard .= "
                 GENDER:F\r\n
-                VCF;
+                ";
             }
             else {
-                $vCard .= <<<VCF
+                $vCard .= "
                 GENDER:O\r\n
-                VCF;
+                ";
             }
         }
 
         if ((boolean)$settings->get('include_phone_numbers', true) === true) {  // Add Phone numbers
             if (!empty(str_replace(' ', '', $profile->phone_private))) {
                 $phone = str_replace(' ', '', $profile->phone_private);
-                $vCard .= <<<VCF
+                $vCard .= "
                 TEL;type=HOME;type=VOICE:$phone\r\n
-                VCF;
+                ";
             }
             if (!empty(str_replace(' ', '', $profile->phone_work))) {
                 $phone = str_replace(' ', '', $profile->phone_work);
-                $vCard .= <<<VCF
+                $vCard .= "
                 TEL;type=WORK;type=VOICE:$phone\r\n
-                VCF;
+                ";
             }
             if (!empty(str_replace(' ', '', $profile->mobile))) {
                 $phone = str_replace(' ', '', $profile->mobile);
-                $vCard .= <<<VCF
+                $vCard .= "
                 TEL;type=CELL;type=VOICE:$phone\r\n
-                VCF;
+                ";
             }
             if (!empty(str_replace(' ', '', $profile->fax))) {
                 $phone = str_replace(' ', '', $profile->fax);
-                $vCard .= <<<VCF
+                $vCard .= "
                 TEL;type=HOME;TYPE=FAX:$phone\r\n
-                VCF;
+                ";
             }
         }
         
         if ((boolean)$settings->get('include_url', true) === true) {  // Add Url's
             if (!empty($profile->url)) {
-                $vCard .= <<<VCF
+                $vCard .= "
                 URL:$profile->url\r\n
-                VCF;
+                ";
             }
         }
 
-        $vCard .= <<<VCF
+        $vCard .= "
         END:VCARD\r\n
-        VCF;
+        ";
         return $vCard;
     }
 
