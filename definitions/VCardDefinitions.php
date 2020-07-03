fa51dd5868cb25fdde8fa1841a1bfd316d12069f
KarlKeu00 <47644344+KarlKeu00@users.noreply.github.com>
2020-07-03 19:31:49 +0200

    Fixing Parsing Error for lower PHP Versions

--- definitions/VCardDefinitions.php
+++ definitions/VCardDefinitions.php
@@ -18,29 +18,23 @@ class VCardDefinitions {
 
         $profile = $user->profile;
 
-        $vCard = "
-        BEGIN:VCARD\r\n
-        VERSION:4.0\r\n
-        KIND:individual\r\n
-        UID:$user->guid\r\n
-        FN:$user->displayname\r\n
-        N:$profile->lastname;$profile->firstname;;;\r\n
-        EMAIL:$user->email\r\n
-        ";
+        $vCard = "BEGIN:VCARD\r\n";
+        $vCard .= "VERSION:4.0\r\n";
+        $vCard .= "KIND:individual\r\n";
+        $vCard .= "UID:$user->guid\r\n";
+        $vCard .= "FN:$user->displayname\r\n";
+        $vCard .= "N:$profile->lastname;$profile->firstname;;;\r\n";
+        $vCard .= "EMAIL:$user->email\r\n";
 
         if ((boolean)$settings->get('include_address', true) === true) {  // Adding Address
-            $vCard .= "
-            ADR;TYPE=home:;;$profile->street;$profile->city;$profile->state;$profile->zip;$profile->country\r\n
-            ";
+            $vCard .= "ADR;TYPE=home:;;$profile->street;$profile->city;$profile->state;$profile->zip;$profile->country\r\n";
         }
 
         if ((boolean)$settings->get('include_profile_image', true) === true) {  // Add Profile Image
             $profileImage = new ProfileImage($user->guid);
             if ($profileImage->hasImage()) {
                 $profileImageUrl = $profileImage->getUrl('', true);
-                $vCard .= "
-                PHOTO:$profileImageUrl\r\n
-                ";
+                $vCard .= "PHOTO:$profileImageUrl\r\n";
             }
         }
         
@@ -54,68 +48,48 @@ class VCardDefinitions {
                 else {
                     $birthdayFormated = $birthdayDatetime->format('Ymd');
                 }
-                $vCard .= "
-                BDAY:$birthdayFormated\r\n
-                ";
+                $vCard .= "BDAY:$birthdayFormated\r\n";
             }
         }
         
         if ((boolean)$settings->get('include_gender', true) === true) {  // Add Gender
             if ($profile->gender === 'male') {
-                $vCard .= "
-                GENDER:M\r\n
-                ";
+                $vCard .= "GENDER:M\r\n";
             }
             else if ($profile->gender === 'female') {
-                $vCard .= "
-                GENDER:F\r\n
-                ";
+                $vCard .= "GENDER:F\r\n";
             }
             else {
-                $vCard .= "
-                GENDER:O\r\n
-                ";
+                $vCard .= "GENDER:O\r\n";
             }
         }
 
         if ((boolean)$settings->get('include_phone_numbers', true) === true) {  // Add Phone numbers
             if (!empty(str_replace(' ', '', $profile->phone_private))) {
                 $phone = str_replace(' ', '', $profile->phone_private);
-                $vCard .= "
-                TEL;type=HOME;type=VOICE:$phone\r\n
-                ";
+                $vCard .= "TEL;type=HOME;type=VOICE:$phone\r\n";
             }
             if (!empty(str_replace(' ', '', $profile->phone_work))) {
                 $phone = str_replace(' ', '', $profile->phone_work);
-                $vCard .= "
-                TEL;type=WORK;type=VOICE:$phone\r\n
-                ";
+                $vCard .= "TEL;type=WORK;type=VOICE:$phone\r\n";
             }
             if (!empty(str_replace(' ', '', $profile->mobile))) {
                 $phone = str_replace(' ', '', $profile->mobile);
-                $vCard .= "
-                TEL;type=CELL;type=VOICE:$phone\r\n
-                ";
+                $vCard .= "TEL;type=CELL;type=VOICE:$phone\r\n";
             }
             if (!empty(str_replace(' ', '', $profile->fax))) {
                 $phone = str_replace(' ', '', $profile->fax);
-                $vCard .= "
-                TEL;type=HOME;TYPE=FAX:$phone\r\n
-                ";
+                $vCard .= "TEL;type=HOME;TYPE=FAX:$phone\r\n";
             }
         }
         
         if ((boolean)$settings->get('include_url', true) === true) {  // Add Url's
             if (!empty($profile->url)) {
-                $vCard .= "
-                URL:$profile->url\r\n
-                ";
+                $vCard .= "URL:$profile->url\r\n";
             }
         }
 
-        $vCard .= "
-        END:VCARD\r\n
-        ";
+        $vCard .= "END:VCARD\r\n";
         return $vCard;
     }
 
